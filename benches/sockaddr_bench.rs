e36b48eb6c67928993c176462c667b589fa61a19
Leonid Shlyapnikov <lshlyapnikov@gmail.com>
2023-01-08 12:36:17 -0500

    Cleanup bench names

--- benches/sockaddr_bench.rs
+++ benches/sockaddr_bench.rs
@@ -6,7 +6,7 @@ use std::collections::hash_map::DefaultHasher;
 use std::hash::Hash;
 use std::hash::Hasher;
 
-fn _benchmark_hash(bench: &mut Bencher) {
+fn benchmark_hash(bench: &mut Bencher) {
     let mut g = quickcheck::Gen::new(1024);
     let x: SockAddr = Arbitrary::arbitrary(&mut g);
     let mut hasher = DefaultHasher::new();
@@ -18,7 +18,7 @@ fn _benchmark_hash(bench: &mut Bencher) {
     })
 }
 
-fn _benchmark_as_socket_hash(bench: &mut Bencher) {
+fn benchmark_as_socket_hash(bench: &mut Bencher) {
     let mut g = quickcheck::Gen::new(1024);
     let x: SockAddr = Arbitrary::arbitrary(&mut g);
     let mut hasher = DefaultHasher::new();
@@ -30,5 +30,5 @@ fn _benchmark_as_socket_hash(bench: &mut Bencher) {
     })
 }
 
-benchmark_group!(benches, _benchmark_hash, _benchmark_as_socket_hash);
+benchmark_group!(benches, benchmark_hash, benchmark_as_socket_hash);
 benchmark_main!(benches);
